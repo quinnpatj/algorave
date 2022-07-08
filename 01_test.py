6b4e6fa9e6249a46f6754f76332d52f88d349d4b
Patrick Quinn <quinnpatj@gmail.com>
2022-07-08 09:56:11 -0500

    Initial commit

---
+++ 01_test.py
@@ -0,0 +1,17 @@
+Scale.default = "minor"
+
+Clock.bpm = 160
+
+d1 >> play("x-o(-=)xxo{o-}")
+
+b1 >> bass([1,3,4], dur=[2,0.5,1], oct=3)
+
+p1 >> pluck([1,3,7], sus=2, amp=0.5)
+p1.every(4, "stutter")
+p1.every(3, "reverse")
+    
+print(SynthDefs)
+
+p2 >> pads([1,3], dur=[8], oct=4)
+
+Clock.clear()
---
+++ foxdot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gnome-terminal -- sclang ~/Documents/algorave/start_foxdot.scd
+sleep 5
+python -m FoxDot
